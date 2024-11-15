d38399ed9079d37d035c5d0994f62512090df3fb
sanasidique <sanasidique29@gmail.com>
2024-11-15 10:12:01 +0530

    Add files via upload

--- singlells.c
+++ singlells.c
@@ -3,55 +3,107 @@
 
 struct node {
     int data;
-    struct node *link;
+    struct node* link;
 };
 
-void add_at_end(struct node *head, int value) {
-    struct node *new_node = malloc(sizeof(struct node));
-    new_node->data = value;
-    new_node->link = NULL;  
+struct node* header = NULL;
 
-    struct node *temp = head;
-    while (temp->link != NULL) {  
+struct node* createnode(int data) {
+    struct node* newnode = malloc(sizeof(struct node));
+    newnode->data = data;
+    newnode->link = NULL;
+    return newnode;
+}
+
+void insertAtFront(int data) {
+    struct node* newnode = createnode(data);
+    if (header == NULL) {
+        header = newnode;
+    } else {
+        newnode->link = header;
+        header = newnode;
+    }
+}
+
+void insertAtLast(int data) {
+    struct node* newnode = createnode(data);
+    if (header == NULL) {
+        header = newnode;
+    } else {
+        struct node* temp = header;
+        while (temp->link != NULL) {
+            temp = temp->link;
+        }
+        temp->link = newnode;
+    }
+}
+
+void insertAtAny(int data, int position) {
+    struct node* newnode = createnode(data);
+    if (position == 0) {
+        newnode->link = header;
+        header = newnode;
+        return;
+    }
+    struct node* temp = header;
+    for (int i = 0; i < position - 1 && temp != NULL; i++) {
         temp = temp->link;
     }
+    if (temp != NULL) {
+        newnode->link = temp->link;
+        temp->link = newnode;
+    } else {
+        printf("Position out of bounds\n");
+    }
+}
 
-    temp->link = new_node;
+void traversal() {
+    struct node* ptr = header;
+    while (ptr != NULL) {
+        printf("%d ", ptr->data);
+        ptr = ptr->link;
+    }
 }
 
 int main() {
-    struct node *head = malloc(sizeof(struct node));
-    head->data = 10;  
-    head->link = NULL;  
+    int choice, data, position;
 
-   
-    struct node *current = malloc(sizeof(struct node));
-    current->data = 100;  
-    current->link = NULL;   list)
+    do {
+        printf("\nMenu:\n");
+        printf("1. Insert at Front\n");
+        printf("2. Insert at Last\n");
+        printf("3. Insert at Any Position\n");
+        printf("4. Traverse the List\n");
+        printf("5. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
 
-    head->link = current;
-
-    
-    printf("%d\n%d\n", head->data, current->data);
-
-    struct node *traverse = head;
-    while (traverse != NULL) {  
-        printf("%d -> ", traverse->data);
-        traverse = traverse->link;
-    }
-
-    printf("NULL\n");
-
-    add_at_end(head, 200);
-
-    printf("\nAfter adding a new element at the end:\n");
-    traverse = head;
-    while (traverse != NULL) {  
-        printf("%d -> ", traverse->data);
-        traverse = traverse->link;
-    }
-
-    printf("NULL\n");
+        switch (choice) {
+            case 1:
+                printf("Enter data to insert at front: ");
+                scanf("%d", &data);
+                insertAtFront(data);
+                break;
+            case 2:
+                printf("Enter data to insert at last: ");
+                scanf("%d", &data);
+                insertAtLast(data);
+                break;
+            case 3:
+                printf("Enter data to insert and position: ");
+                scanf("%d %d", &data, &position);
+                insertAtAny(data, position);
+                break;
+            case 4:
+                traversal();
+                break;
+            case 5:
+                printf("Exiting...\n");
+                break;
+            default:
+                printf("Invalid choice!\n");
+        }
+    } while (choice != 5);
 
     return 0;
 }
